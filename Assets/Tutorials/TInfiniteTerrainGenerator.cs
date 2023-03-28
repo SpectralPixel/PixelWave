8f5d8bdeb823763b3ea42d8e7aa7f0d909cdce46
SpectralPixel <encrypteddvjjrxv@gmail.com>
2023-03-28 22:10:23 +0200

    Implemented infdev tutorial
    still need to adapt my own code though

---
+++ Assets/Tutorials/TInfiniteTerrainGenerator.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TInfiniteTerrainGenerator : MonoBehaviour
+{
+
+    [SerializeField] private Transform player;
+    [SerializeField] private int renderDistance;
+    private TWorldGenerator GeneratorInstance;
+    private List<Vector2Int> CoordsToRemove;
+
+    void Start()
+    {
+        GeneratorInstance = GetComponent<TWorldGenerator>();
+        CoordsToRemove = new List<Vector2Int>();
+    }
+
+    void Update()
+    {
+        //converts player to chunk coords
+        int _playerChunkX = (int)player.position.x / TWorldGenerator.ChunkSize.x;
+        int _playerChunkZ = (int)player.position.z / TWorldGenerator.ChunkSize.z;
+        CoordsToRemove.Clear();
+
+        foreach(KeyValuePair<Vector2Int, GameObject> _activeChunk in TWorldGenerator.ActiveChunks)
+        {
+            CoordsToRemove.Add(_activeChunk.Key);
+        }
+
+        for (int x = _playerChunkX - renderDistance; x <= _playerChunkX + renderDistance; x++)
+        {
+            for (int y = _playerChunkZ - renderDistance; y <= _playerChunkZ + renderDistance; y++)
+            {
+                Vector2Int _chunkCoord = new Vector2Int(x, y);
+                if (!TWorldGenerator.ActiveChunks.ContainsKey(_chunkCoord)) GeneratorInstance.CreateChunk(_chunkCoord);
+
+                CoordsToRemove.Remove(_chunkCoord);
+            }
+        }
+
+        foreach (Vector2Int _coord in CoordsToRemove)
+        {
+            GameObject _chunkToDelete = TWorldGenerator.ActiveChunks[_coord];
+            TWorldGenerator.ActiveChunks.Remove(_coord);
+            Destroy(_chunkToDelete);
+        }
+
+    }
+}
--- Assets/Tutorials/TWorldGenerator.cs
+++ Assets/Tutorials/TWorldGenerator.cs
@@ -1,31 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
 using UnityEngine;
 
 public class TWorldGenerator : MonoBehaviour
 {
-    [SerializeField] private TTextureLoader TextureLoaderInstance;
-    [Space]
+    public static Dictionary<Vector3Int, int[,,]> WorldData;
+    public static Dictionary<Vector2Int, GameObject> ActiveChunks;
     public static readonly Vector3Int ChunkSize = new Vector3Int(16, 256, 16);
+    [Space]
+    [SerializeField] private TTextureLoader TextureLoaderInstance;
+    public Material _meshMaterial;
+    [Space]
     public Vector2 NoiseScale = Vector2.one;
     public Vector2 NoiseOffset = Vector2.zero;
     [Space]
     public int HeightOffset = 60;
     public float HeightIntensity = 5;
-    [Space]
-    public Material MeshMaterial;
-    private int[,,] tempData; //int[,,] is a 3 dimensional int array
+
+    private TChunkMeshCreator meshCreator;
 
     void Start()
     {
-        tempData = new int[ChunkSize.x, ChunkSize.y, ChunkSize.z];
+        WorldData = new Dictionary<Vector3Int, int[,,]>();
+        ActiveChunks = new Dictionary<Vector2Int, GameObject>();
+
+        meshCreator = new TChunkMeshCreator(TextureLoaderInstance);
+
+        for (int x = -2; x < 2; x++)
+        {
+            for (int y = -2; y < 2; y++)
+            {
+                CreateChunk(new Vector2Int(x, y));
+            }
+        }
+    }
+
+    public void CreateChunk(Vector2Int _chunkCoord)
+    {
+        Vector3Int _position = new Vector3Int(_chunkCoord.x, 0, _chunkCoord.y);
+        int[,,] dataToApply = WorldData.ContainsKey(_position) ? WorldData[_position] : GenerateData(_position);
+
+        string _newChunkName = $"Chunk {_chunkCoord.x} {_chunkCoord.y}";
+        GameObject _newChunk = new GameObject(_newChunkName, new System.Type[]
+        {
+            typeof (MeshRenderer),
+            typeof(MeshFilter),
+            typeof (MeshCollider)
+        });
+
+        ////////////////////////////////////////////// https://youtu.be/Y8LCHU3hi2U?list=PLEvnA6UOOVbkOTaFPe4P0RA3W1mINbWCf&t=321
+        MeshRenderer _newChunkRenderer = _newChunk.GetComponent<MeshRenderer>();
+        MeshFilter _newChunkFilter = _newChunk.GetComponent<MeshFilter>();
+        MeshCollider _newChunkCollider = _newChunk.GetComponent<MeshCollider>();
+
+        _newChunk.transform.position = new Vector3(_chunkCoord.x * 16f, 0f, _chunkCoord.y * 16f);
+        _newChunkRenderer.material = _meshMaterial;
+        _newChunkFilter.mesh = meshCreator.CreateMeshFromData(dataToApply);
+        _newChunkCollider.sharedMesh = _newChunkFilter.mesh;
+        ActiveChunks.Add(_chunkCoord, _newChunk);
+    }
+
+    public int[,,] GenerateData(Vector3Int _offset)
+    {
+        int[,,] _tempData = new int[ChunkSize.x, ChunkSize.y, ChunkSize.z]; //int[,,] is a 3 dimensional int array
 
         for (int x = 0; x < ChunkSize.x; x++)
         {
             for (int z = 0; z < ChunkSize.z; z++)
             {
-                float PerlinCoordX = NoiseOffset.x + x / (float)ChunkSize.x * NoiseScale.x;
-                float PerlinCoordY = NoiseOffset.y + z / (float)ChunkSize.z * NoiseScale.y;
+                float PerlinCoordX = NoiseOffset.x + (x + (_offset.x * 16f)) / (float)ChunkSize.x * NoiseScale.x;
+                float PerlinCoordY = NoiseOffset.y + (z + (_offset.z * 16f)) / (float)ChunkSize.z * NoiseScale.y;
                 int HeightGen = Mathf.RoundToInt(Mathf.PerlinNoise(PerlinCoordX, PerlinCoordY) * HeightIntensity + HeightOffset);
 
                 for (int y = HeightGen; y >= 0; y--)
@@ -44,50 +89,13 @@ public class TWorldGenerator : MonoBehaviour
                     // height 0 is bedrock
                     if (y == 0) _blockTypeToAssign = 4;
 
-                    tempData[x, y, z] = _blockTypeToAssign;
+                    _tempData[x, y, z] = _blockTypeToAssign;
                 }
             }
         }
 
-        GameObject _tempChunk = new GameObject("Chunk", new System.Type[] { typeof(MeshFilter), typeof(MeshRenderer) });
-        _tempChunk.GetComponent<MeshFilter>().mesh = new TChunkMeshCreator(TextureLoaderInstance).CreateMeshFromData(tempData);
-        _tempChunk.GetComponent<MeshRenderer>().material = MeshMaterial;
+        WorldData.Add(_offset, _tempData);
+        return _tempData;
     }
 
-    /*private void OnDrawGizmos()
-    {
-        if (tempData != null)
-        {
-            for (int x = 0; x < ChunkSize.x; x++)
-            {
-
-                for (int y = 0; y < ChunkSize.y; y++)
-                {
-
-                    for (int z = 0; z < ChunkSize.z; z++)
-                    {
-                        switch(tempData[x, y, z])
-                        {
-                            case 0:
-                                continue;
-                            case 1:
-                                Gizmos.color = Color.green;
-                                break;
-                            case 2:
-                                Gizmos.color = Color.red;
-                                break;
-                            case 3:
-                                Gizmos.color = Color.gray;
-                                break;
-                            case 4:
-                                Gizmos.color = Color.black;
-                                break;
-                        }
-
-                        Gizmos.DrawWireCube(new Vector3(x, y, z), Vector3.one);
-                    }
-                }
-            }
-        }
-    }*/
 }
